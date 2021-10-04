cd86538e2043389b5d21c2f31fd5e4fe3286587f
pradyumnauma <91915459+pradyumnauma@users.noreply.github.com>
2021-10-05 01:32:37 +0530

    intial commit

---
+++ navbar.php
@@ -0,0 +1,23 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #C0392B;"><b>TSF BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #F64C72;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #F64C72;"><b>Create User</b></a>
+              </li>
+               <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #F64C72;"><b>View all Customers</b></a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #F64C72;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
